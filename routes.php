9c2896115d27d61a5a242d647b33337c0c53b8ae
Alan4247748 <murphy@pbranding.de>
2024-12-29 15:21:06 -0600

    S

--- routes.php
+++ routes.php
@@ -1,4 +1,44 @@
 <?php
+session_start();
+
+// Helper functions
+function loadProducts() {
+    return json_decode(file_get_contents('products.json'), true);
+}
+
+function addToCart($productId, $quantity) {
+    $cart = $_SESSION['cart'] ?? [];
+    $cart[$productId] = ($cart[$productId] ?? 0) + $quantity;
+    $_SESSION['cart'] = $cart;
+}
+
+function clearCart() {
+    $_SESSION['cart'] = [];
+}
+
+function getCartItems() {
+    $products = loadProducts();
+    $cart = $_SESSION['cart'] ?? [];
+    $cartItems = [];
+
+    foreach ($cart as $id => $quantity) {
+        foreach ($products as $product) {
+            if ($product['product_id'] == $id) {
+                $cartItems[] = [
+                    'name' => $product['name'],
+                    'quantity' => $quantity,
+                    'price' => $product['price'],
+                    'total' => $product['price'] * $quantity
+                ];
+                break;
+            }
+        }
+    }
+
+    return $cartItems;
+}
+
+// Route handling
 $routes = [
     'home' => function () {
         include 'index.html';
@@ -20,7 +60,7 @@ $routes = [
     },
     'api/products' => function () {
         header('Content-Type: application/json');
-        echo file_get_contents('products.json');
+        echo json_encode(loadProducts());
         exit;
     },
     'cart/add' => function () {
@@ -37,6 +77,20 @@ $routes = [
         clearCart();
         header('Location: /cart');
         exit;
+    },
+    'cart/items' => function () {
+        header('Content-Type: application/json');
+        echo json_encode(getCartItems());
+        exit;
     }
 ];
+
+// Route dispatcher
+$page = $_GET['page'] ?? 'home';
+if (isset($routes[$page])) {
+    $routes[$page]();
+} else {
+    http_response_code(404);
+    echo "404 - Page Not Found";
+}
 ?>
